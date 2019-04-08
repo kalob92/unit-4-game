e9efe14847c0219354d6bd4681352ef76630d75a
Kalob <kalobcarter@Kalobs-MacBook-Pro.local>
2019-04-07 22:03:00 -0600

    css is finished

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+var randomNumber;
+var winCount = 0;
+var lossCount = 0;
+var crystal1;
+var crystal2;
+var crystal3;
+var crystal4;
+var totalScore = 0;
+
+// random-number is held by random-number-box and is a random number generated after a win or loss scenario
+// is a number between 19 and 120
+
+
+// win-count counts wins and will replace the 0 in the HTML
+
+
+// loss-count counts losses and will replace the 0 in the HTML
+
+
+// crystal-one crystal-two crystal-three crystal-four each will have a random hidden value between 1 and 12
+
+
+// total-score counts the crystal total and will replace the 0 in the HTML
