2d5c6b7de8b76275e98a4ea6a593cd5498c2dcf6
Kalob <kalobcarter@Kalobs-MacBook-Pro.local>
2019-04-07 23:48:26 -0600

    much more jquery added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,55 @@
-var randomNumber;
-var winCount = 0;
-var lossCount = 0;
-var crystal1;
-var crystal2;
-var crystal3;
-var crystal4;
-var totalScore = 0;
+$(document).ready(function() {
+    var winCount = 0;
+    var winCountE = document.getElementById("win-count");
+    var lossCount = 0;
+    var lossCountE = document.getElementById("loss-count");
+    var crystal1 = document.getElementById("crystal-one");
+    var crystal2 = document.getElementById("crystal-two");
+    var crystal3 = document.getElementById("crystal-three");
+    var crystal4 = document.getElementById("crystal-four");
+    var totalScore = 0;
+    var totalScoreE = document.getElementById("total-score");
 
-// random-number is held by random-number-box and is a random number generated after a win or loss scenario
-// is a number between 19 and 120
+    // random-number is held by random-number-box and is a random number generated after a win or loss scenario
+    // is a number between 19 and 120
+    var randomNumber = Math.floor(Math.random() * 102) + 19;
+    console.log("random number is " + randomNumber);
+
+    // this is taking the random-number element and making it a variable so that it may be rewritten
+    var randomNumberE = document.getElementById("random-number");
+
+    // this will replace the 0 in the random-number box w the number created by randomNumber
+    randomNumberE.innerText = randomNumber;
+    
+
+    // win-count counts wins and will replace the 0 in the HTML
+    winCountE.innerText = winCount;
+
+    // loss-count counts losses and will replace the 0 in the HTML
+    lossCountE.innerText = lossCount;
 
 
-// win-count counts wins and will replace the 0 in the HTML
+    // crystal-one crystal-two crystal-three crystal-four each will have a random hidden value between 1 and 12
+    var value1 = Math.floor(Math.random() * 11) + 1;
+    console.log("crystal one is worth " + value1);
+
+    var value2 = Math.floor(Math.random() * 11) + 1;
+    console.log("crystal two is worth " + value2);
+
+    var value3 = Math.floor(Math.random() * 11) + 1;
+    console.log("crystal three is worth " + value3);
+
+    var value4 = Math.floor(Math.random() * 11) + 1;
+    console.log("crystal four is worth " + value4);
 
 
-// loss-count counts losses and will replace the 0 in the HTML
+    $("#crystal-one").on('click', function(e) {
+        totalScore += value1;
+        console.log("the total is " + totalScore);
+    })
 
+    // total-score counts the crystal total and will replace the 0 in the HTML
+    totalScoreE.innerText = totalScore;
+    console.log("the total score is " + totalScore);
 
-// crystal-one crystal-two crystal-three crystal-four each will have a random hidden value between 1 and 12
-
-
-// total-score counts the crystal total and will replace the 0 in the HTML
+})
